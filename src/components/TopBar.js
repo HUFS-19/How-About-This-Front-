7e9fc195d4ed4c970958a8ebe8f80a8c05abdfca
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-07-23 21:19:35 +0900

    style: top bar 구현

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,6 +6,21 @@ import '../styles/components/_TopBar.scss';
 const TopBar = () => {
   return (
     <div className='TopBar'>
+      <select className='category-selector'>
+        <option value='all'>전체</option>
+        <option value='like'>좋아요 목록</option>
+        <option value='category1'>카테고리 1</option>
+        <option value='category2'>카테고리 2</option>
+        <option value='category3'>카테고리 3</option>
+        <option value='category4'>카테고리 4</option>
+        <option value='category5'>카테고리 5</option>
+        <option value='category6'>카테고리 6</option>
+      </select>
+      <select className='search-selector'>
+        <option value='product'>상품명</option>
+        <option value='uploader'>작성자명</option>
+        <option value='tag'>태그명</option>
+      </select>
       <div className='search_wrapper'>
         <FontAwesomeIcon className='search-icon' icon={faSearch} />
         <input
