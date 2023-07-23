3e62e3ad07636f5a66306e4f215ed5fcbe7afc64
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-07-23 23:28:48 +0900

    style: 상품 box 구현

--- src/components/ProductBlock.js
+++ src/components/ProductBlock.js
@@ -1,11 +1,16 @@
 import fan from '../assets/fan.png';
+import doll from '../assets/doll.jpeg';
 
 import '../styles/components/_ProductBlock.scss';
 
 const ProductBlock = ({ data }) => {
   return (
     <div className='ProductBlock'>
-      <img className='product-img' src={fan} alt='' />
+      <img
+        className='product-img'
+        src={data.id % 2 === 0 ? fan : doll}
+        alt=''
+      />
       <p className='product-name'>{data.name}</p>
     </div>
   );
