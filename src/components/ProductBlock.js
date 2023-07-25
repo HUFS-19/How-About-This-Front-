7697dd32255cec2120a472037fa4a706a40c99d3
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-07-25 11:18:12 +0900

    feat: 좋아요 버튼 구현

--- src/components/ProductBlock.js
+++ src/components/ProductBlock.js
@@ -1,15 +1,37 @@
+import { useState, useRef } from 'react';
+
 import fan from '../assets/fan.png';
 import doll from '../assets/doll.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 import '../styles/components/_ProductBlock.scss';
 
 const ProductBlock = ({ data }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const heartIcon = useRef();
+
+  if (heartIcon.current) {
+    if (clicked && !heartIcon.current.classList.contains('clicked')) {
+      heartIcon.current.classList.add('clicked');
+    } else if (!clicked && heartIcon.current.classList.contains('clicked')) {
+      heartIcon.current.classList.remove('clicked');
+    }
+  }
+
   return (
     <div className='ProductBlock'>
       <img
         className='product-img'
         src={data.id % 2 === 0 ? fan : doll}
         alt=''
+      />
+      <FontAwesomeIcon
+        ref={heartIcon}
+        onClick={() => setClicked(!clicked)}
+        className='heart-icon'
+        icon={faHeart}
       />
       <p className='product-name'>{data.name}</p>
     </div>
