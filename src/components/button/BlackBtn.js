0fd9b872486a4c39e2d8df9f923d49ecc0270c89
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-08-04 16:46:26 +0900

    fix: 버튼 컴포넌트에 onClick 이벤트 추가

--- src/components/button/BlackBtn.js
+++ src/components/button/BlackBtn.js
@@ -2,8 +2,11 @@ import '../../styles/components/button/_BlackBtn.scss';
 
 const BlackBtn = ({ onClick, id, text }) => {
   return (
-    // <button onClick={() => window.open(onClick)} id={id} className='BlackBtn'>
-    <button id={id} className='BlackBtn'>
+    <button
+      onClick={onClick ? () => window.open(onClick) : () => {}}
+      id={id}
+      className='BlackBtn'
+    >
       {text}
     </button>
   );
