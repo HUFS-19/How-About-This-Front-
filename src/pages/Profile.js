b3e9a069ffe85ac3632be65cb841e59b9af45b1c
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-08-14 11:24:00 +0900

    feat: 상품 추가 버튼 클릭 시 업로드 페이지로 이동 구현

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import '../styles/components/profile/_ProfileArea.scss';
 import ProfileProdList from '../components/profile/ProfileProdList';
@@ -8,6 +8,8 @@ import UserProfile from '../components/profile/UserProfile';
 
 const Profile = () => {
   const { userId } = useParams();
+
+  const navigate = useNavigate();
 
   const [profileData, setProfileData] = useState({});
   const [snsData, setSnsData] = useState({});
@@ -36,7 +38,12 @@ const Profile = () => {
           ></UserProfile>
 
           {isLogin ? (
-            <button className='addProductBtn'>신규 상품 추가</button>
+            <button
+              className='addProductBtn'
+              onClick={() => navigate('/upload')}
+            >
+              신규 상품 추가
+            </button>
           ) : (
             <hr />
           )}
